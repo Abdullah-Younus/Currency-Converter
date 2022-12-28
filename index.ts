130ec9c0e38513894f81aa50a6ae0568e283867c
Abdullah-Younus <abdullah1001k@gmail.com>
2022-12-28 20:25:39 +0500

    local changes

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+
+let Converters = {
+    "PKR": {
+        "USD": 0.0044,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    "GBP": {
+        "USD": 1.21,
+        "PKR": 271.79,
+        "GBP": 1
+    },
+    "USD": {
+        "PKR": 225.50,
+        "GBP": 0.83,
+        "USD": 1
+    }
+}
+
+const answer: { from: string, to: string, amount: number } = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'from',
+        choices: ["PKR", "GBP", "USD"],
+        message: "Select Your Currency: "
+    },
+    {
+        type: 'list',
+        name: 'to',
+        choices: ["PKR", "GBP", "USD"],
+        message: "Select Your Convert  Currency:"
+    },
+    {
+        type: 'number',
+        name: 'amount',
+        message: 'Enter Your convertion amount:'
+    }
+])
